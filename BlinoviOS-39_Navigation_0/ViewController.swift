4fc1a72894d7f9b59bae719f1f0b78c24111d75a
Illiyanibl <illiyanbn@icloud.com>
2023-09-26 11:20:19 +0400

    Initial commit

---
+++ BlinoviOS-39_Navigation_0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlinoviOS-39_Navigation_0
+//
+//  Created by Illya Blinov on 26.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
