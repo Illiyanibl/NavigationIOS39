cb0db3f9f87ea01fba7cfab8ab84a162bea435a5
Illiyanibl <illiyanbn@icloud.com>
2023-09-26 14:17:53 +0400

    Complited 5 tasks

---
+++ BlinoviOS-39_Navigation_0/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  BlinoviOS-39_Navigation_0
+//
+//  Created by Illya Blinov on 26.09.23.
+//
+
+import UIKit
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+        title = "Profile"
+    }
+}
--- BlinoviOS-39_Navigation_0/SceneDelegate.swift
+++ BlinoviOS-39_Navigation_0/SceneDelegate.swift
@@ -13,10 +13,32 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: scene)
+
+        let tabMainViewController = UITabBarController()
+        let feedViewController = FeedViewController()
+        let profileViewController = ProfileViewController()
+        tabMainViewController.tabBar.barStyle = .default
+        tabMainViewController.tabBar.backgroundColor = .white
+        tabMainViewController.tabBar.tintColor = .systemRed
+        tabMainViewController.tabBar.unselectedItemTintColor = .systemGray3
+
+        let profileNavigationController = UINavigationController(rootViewController: profileViewController)
+
+        let feedNavigationController = UINavigationController(rootViewController: feedViewController)
+
+
+        profileNavigationController.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(named: "profile") , tag: 1)
+        feedNavigationController.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(named: "feed"), tag: 0)
+
+        tabMainViewController.viewControllers = [feedNavigationController,profileNavigationController]
+        tabMainViewController.selectedIndex = 0
+
+        window.rootViewController = tabMainViewController
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
