77ab1a4d6f40036f1a6511d6108720ed7865aa5a
Illiyanibl <illiyanbn@icloud.com>
2023-10-02 18:51:38 +0400

    Was corrected  mistakes

--- BlinoviOS-39_Navigation_0/ProfileViewController.swift
+++ BlinoviOS-39_Navigation_0/ProfileViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .gray
+        view.backgroundColor = .systemGray
         title = "Profile"
     }
 }
